668b95b17e9bc1583b14585d0cc57a07f1a4860e
0rmindo <86755009+0rmindo@users.noreply.github.com>
2021-10-18 16:23:24 -0300

    Create seller signup feature with Ajax

---
+++ scripts/createSeller.js
@@ -0,0 +1,20 @@
+function createSeller(event) {
+  event.preventDefault();
+
+  const data = $('form').serialize();
+
+  $.ajax({
+    type: 'POST',
+    dataType: 'json',
+    url: 'controllers/sellerSignup.php',
+    async: true,
+    data: data,
+    success: function(response) {
+      if (response.success) {
+        window.location.replace(`views/homeSeller.php?name=${response.user.name}`);
+      } else {
+        alert('Desculpe, não foi possível realizar o cadastro. Utilize outros dados e tente novamente.');
+      }
+    }
+  });
+}
